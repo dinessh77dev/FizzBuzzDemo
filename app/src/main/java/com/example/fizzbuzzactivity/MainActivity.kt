2d6383f59a1f25b3db0c5bd959d23cc8241fe2dc
dinesh <dinessh77@gmail.com>
2021-04-23 12:48:41 +0530

    <Code Refactor>  code refactored to remove code duplication.

--- app/src/main/java/com/example/fizzbuzzactivity/MainActivity.kt
+++ app/src/main/java/com/example/fizzbuzzactivity/MainActivity.kt
@@ -10,10 +10,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun fizzBuzz(number: Int): String {
+        if (number == 0) return "$number"
         return when {
-            number == 0 -> "$number"
-            number % 3 == 0 && number % 5 == 0 -> "FIZZBUZZ"
-            number % 3 == 0 -> "FIZZ"
+            number % 3 == 0 -> "FIZZ${if (number % 5 == 0) "BUZZ" else ""}"
             number % 5 == 0 -> "BUZZ"
             else -> "$number"
         }

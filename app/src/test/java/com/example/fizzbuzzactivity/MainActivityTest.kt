22679250120bea426ee577958e0789f143c5777b
dinesh <dinessh77@gmail.com>
2021-04-23 12:12:34 +0530

    <Test Case> initial test case to test return value of 0.

--- app/src/test/java/com/example/fizzbuzzactivity/MainActivityTest.kt
+++ app/src/test/java/com/example/fizzbuzzactivity/MainActivityTest.kt
@@ -1,6 +1,15 @@
 package com.example.fizzbuzzactivity
 
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
 class MainActivityTest {
 
+    private val activity = MainActivity()
+
+    @Test
+    fun test_value_0() {
+        assertEquals("0",activity.fizzBuzz(0))
+    }
 
 }

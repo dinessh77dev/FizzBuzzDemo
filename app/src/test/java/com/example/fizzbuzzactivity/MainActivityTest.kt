7b5af11d5f3fb44524f8f30e445fe481f649db4e
dinesh <dinessh77@gmail.com>
2021-04-23 13:12:35 +0530

    <Code Refactor> replace hard coded strings with const in test class

--- app/src/test/java/com/example/fizzbuzzactivity/MainActivityTest.kt
+++ app/src/test/java/com/example/fizzbuzzactivity/MainActivityTest.kt
@@ -9,36 +9,42 @@ class MainActivityTest {
 
     @Test
     fun test_value_0() {
-        assertEquals("0",activity.fizzBuzz(0))
+        assertEquals("0", activity.fizzBuzz(0))
     }
 
     @Test
-    fun test_value_of_1(){
-        assertEquals("1",activity.fizzBuzz(1))
+    fun test_value_of_1() {
+        assertEquals("1", activity.fizzBuzz(1))
     }
 
     @Test
-    fun test_value_of_3(){
-        assertEquals("FIZZ",activity.fizzBuzz(3))
+    fun test_value_of_3() {
+        assertEquals(FIZZ, activity.fizzBuzz(3))
     }
 
     @Test
-    fun test_value_of_5(){
-        assertEquals("BUZZ",activity.fizzBuzz(5))
+    fun test_value_of_5() {
+        assertEquals(BUZZ, activity.fizzBuzz(5))
     }
 
     @Test
-    fun test_value_of_6(){
-        assertEquals("FIZZ",activity.fizzBuzz(6))
+    fun test_value_of_6() {
+        assertEquals(FIZZ, activity.fizzBuzz(6))
     }
 
     @Test
-    fun test_value_of_10(){
-        assertEquals("BUZZ",activity.fizzBuzz(10))
+    fun test_value_of_10() {
+        assertEquals(BUZZ, activity.fizzBuzz(10))
     }
 
     @Test
-    fun test_value_of_15(){
-        assertEquals("FIZZBUZZ",activity.fizzBuzz(15))
+    fun test_value_of_15() {
+        assertEquals(FIZZBUZZ, activity.fizzBuzz(15))
+    }
+
+    companion object {
+        const val FIZZ = "FIZZ"
+        const val BUZZ = "BUZZ"
+        const val FIZZBUZZ = "FIZZBUZZ"
     }
 }
